3ac89d34d73d3b124c3e7bb3ff886c3277eb6301
Sirus10 <coindre_e@hotmail.fr>
2020-11-21 16:01:45 +0100

    Update manage_tydom.sh

--- tydom_python_domo/manage_tydom.sh
+++ tydom_python_domo/manage_tydom.sh
@@ -1,4 +1,4 @@
-#
+#!/usr/bin/ksh
 # Just a simple way to play with tydom main.py file
 #
 # You can use same arguiment than in main.py
@@ -14,15 +14,62 @@
 #    ./manage_tydom.sh put_devices_data 1234123 10
 #
 #	
+DOMOTICZ=localhost:8080
+DIR=/root/Tydom
 
-# Step 1 Run the command
-python main.py $1 $2 $3 $4 >  config.json
+# Step 1 Run the command to get latest input and manage devices 
+python $DIR/main.py $1 $2 $3 $4 >  $DIR/config.json
 
 
-# Step 3 Merge config/status generated
-echo "UPDATE : `date`" > status.txt
-grep -v DEB config.json >> status.txt
+# Step 2 Merge config/status generated
+grep -v DEB $DIR/config.json  |grep ":" > $DIR/status.txt
 
 
 # Step x Display the file / just for fun
-cat status.txt
+cat $DIR/status.txt
+
+
+# Update one by one each devices in domoticz
+while IFS="|" read -r TYDOM_ID INIT_NAME DOMO_ID_VAL DOMO_ID_CMD NAME
+do 
+	# On recuperer la valeur dans le fichier status.txt pour cet ID
+	VALUDOMO=`grep $TYDOM_ID $DIR/status.txt |grep : | cut -d':' -f2 | sed 's/ //'`
+
+	# On met a jour le compteur % dans domoticz
+	curl --silent --max-time 5 "http://$DOMOTICZ/json.htm?type=command&param=udevice&idx=$DOMO_ID_VAL&nvalue=0&svalue=$VALUDOMO" >>/dev/null
+
+	# On recupere le nom du selecteur correspondant 
+	# NAMEONLY=`curl --max-time 5 -s  "http://192.168.1.103:8080/json.htm?type=devices&rid=$DOMO_ID_CMD" |grep '"Name"' |cut -d'"' -f4 |cut -d'-' -f1`
+	
+	# if [ "$NAMEONLY" != '' ] ; then 
+		#On le renmone en modifiant le % 
+		NewName=`echo "$NAME+-+$VALUDOMO%25" |sed "s/ /+/g"`
+		# NewName=$NAME
+		echo NAMEONLY : $NAMEONLY
+		echo NewName: $NewName
+		curl --silent --max-time 5 "http://$DOMOTICZ/json.htm?type=setused&idx=$DOMO_ID_CMD&name=$NewName&used=true" >>/dev/null
+	# fi
+
+	#On modifi la valeur du selecteur pour ne pas qu'il affiche la derniere valeur (undefined) 
+	curl --silent --max-time 5 "http://$DOMOTICZ/json.htm?type=command&param=udevice&idx=$DOMO_ID_CMD&nvalue=100&svalue=5" >>/dev/null
+
+done < $DIR/config.txt
+
+rm $DIR/config.json
+
+## Temp passé
+hrs=$(( SECONDS/3600 )); mins=$(( (SECONDS-hrs*3600)/60)); secs=$(( SECONDS-hrs*3600-mins*60 ))
+printf 'Time spent: %02d:%02d:%02d\n' $hrs $mins $secs
+
+
+
+
+
+
+
+
+
+
+
+
+
